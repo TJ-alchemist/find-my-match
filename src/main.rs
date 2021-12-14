773b039dccc37f098e951a1494c9198a6e7a4b8a
Tejas <tejasvijay09@gmail.com>
2021-12-14 19:28:37 +0530

    A: Command line code for accepting data

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,91 @@
 use rand::Rng;
 use std::env;
+use std::collections::HashMap;
+use std::io;
+
 
 #[derive(Debug)]
-enum GenderType {
-  	Male,
+enum Gender {
+    Male,
   	Female,
 	Other
 }
 
+//#[derive(Debug)]
+//enum AgeGroup {
+//    Youth,
+//    Adults,
+//    Seniors
+//}
+//
 #[derive(Debug)]
 struct PersonDetails {
   	name: String,
-  	gender_type: GenderType
+    user_name: String,
+    age: u8,
+  	gender: Gender,
+//    preference: Option<HashMap<AgeGroup, Vec<PersonDetails>>>
 }
 
-fn search_match(person_details: PersonDetails){
-	let males = ["John", "Nicolas", "Tom", "Jake", "Brandon", "Robert"];
-	let females = ["Sarah", "Gal", "Angelina", "Jennifer", "Monica", "Stacy"];
-	let rng = rand::thread_rng().gen_range(0..6);
-	match person_details.gender_type {
-		GenderType::Male => println!("{:?} has a match with {:?}", person_details.name, females[rng]),
-		GenderType::Female => println!("{:?} has a match with {:?}", person_details.name, males[rng]),
-		GenderType::Other => println!("We don't know what gender type this person is interested in"),
-	}	
-}
+//impl PersonDetails {
+//    fn new_user(&self) -> Self {
+//        let mut fields: u8 = 4;
+//        while fields != 0 {
+//            
+//            io::stdin()
+//                .read_line(&mut self.name)
+//                .expect("Something went wrong");
+//            fields -= 1;
+//        }
+//
+//        Self {
+//            name: self.name,
+//            user_name: self.user_name,
+//            age: self.age,
+//            gender: self.gender,
+//            preference: self.preference
+//        }
+//    }
+//}
 
-fn main(){
-	let  cli_name: String = env::args().nth(1).expect("Please provide a name!");
-	let  cli_gender: String = env::args().nth(2).expect("Please provide your gender");
-	let male: String = String::from("M");
-	let female: String = String::from("F");
-	let others: String = String::from("O");
-	let cli_gender_mapped: GenderType = match cli_gender {
-		male => GenderType::Male,
-		female => GenderType::Female,
-		others => GenderType::Other,
-	};
-  	let detail = PersonDetails {
-		name: cli_name,
-		gender_type: cli_gender_mapped,
-  	};
-  	search_match(detail);
+fn main() {
+//    let users: Vec<PersonDetails> = Vec::new();
+    println!("Welcome to Find-My-Match");
+    println!("Lets start..");
+    // Accepting name of the user
+    println!("\nEnter your name:");
+    let mut name: String = String::new();
+    io::stdin().read_line(&mut name).expect("Could not read name");
+    name = name.trim().to_string();
+    // Accepting user_name of the user
+    println!("\nEnter a user_name:");
+    let mut user_name: String = String::new();
+    io::stdin().read_line(&mut user_name).expect("Could not read user_name");
+    user_name = user_name.trim().to_string();
+    // Accepting age of the user
+    println!("\nEnter your age:");
+    let mut age: String = String::new();
+    io::stdin().read_line(&mut age).expect("Could not read age");
+    let age: u8 = age.trim().parse().expect("Could not parse age");
+    // Accepting gender of user
+    println!("\nEnter gender (M/F/O):");
+    let mut gender: String = String::new();
+    io::stdin().read_line(&mut gender).expect("Could not read gender");
+    gender = gender.trim().to_string();
+    
+    let user = PersonDetails {
+        name: name,
+        user_name: user_name,
+        age: age,
+        gender: if gender == "M" {
+            Gender::Male
+        } else if gender == "F" {
+            Gender::Female
+        } else {
+            Gender::Other
+        }
+    };
+
+    println!("{:?}", user);
+
 }
