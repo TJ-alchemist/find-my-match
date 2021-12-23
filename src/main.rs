6812f1179b639fae4e7c02754d8fa7a2eba60116
Tejas <tejasvijay09@gmail.com>
2021-12-23 14:53:08 +0530

    Created different modules for the code

--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,82 @@
 use std::io;
-use std::fs::{File, OpenOptions, write};
-use serde::{Serialize, Deserialize};
 use std::path::Path;
-use std::io::{Read, BufReader};
 use serde_json;
-use std::error::Error;
-use std::fs;
+mod utils;
 
-#[derive(Debug, Serialize, Deserialize, PartialEq)]
-enum Gender {
-    Male,
-  	Female
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct PersonDetails {
-  	name: String,
-    user_name: String,
-    age: u8,
-  	gender: Gender,
-    preferences: Vec<String>
-}
-    
-fn file_writer<P: AsRef<Path>, C: AsRef<[u8]>>(path: P, contents: C) -> io::Result<()> {
-    fs::write(path, &contents)?;
-    Ok(())
-}
-
-fn read_user_from_file<P: AsRef<Path>>(path: P) -> Result<Vec<PersonDetails>, Box<dyn Error>> {
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-    let u: Vec<PersonDetails> = serde_json::from_reader(reader)?;
-    Ok(u)
-}
 
 fn main()  {
-
-    // let users: Vec<PersonDetails> = Vec::new();
-    println!("Welcome to Find-My-Match");
-    println!("Lets start..");
-
-    println!("Are you already registered? [y/n]");
-    let mut registered: String = String::new();
-    io::stdin().read_line(&mut registered).expect("Something went wrong");
-    registered = registered.trim().to_string();
-
-    let mut all_users = read_user_from_file("db.json").unwrap();
-    let s1: String = String::from("Hello");
-    if registered == "n" {
-        // Accepting name of the user
-        println!("\nEnter your name:");
-        let mut name: String = String::new();
-        io::stdin().read_line(&mut name).expect("Could not read name");
-        name = name.trim().to_string();
-        // Accepting user_name of the user
-        println!("\nEnter a user_name:");
-        let mut user_name: String = String::new();
-        io::stdin().read_line(&mut user_name).expect("Could not read user_name");
-        user_name = user_name.trim().to_string();
-        // Accepting age of the user
-        println!("\nEnter your age:");
-        let mut age: String = String::new();
-        io::stdin().read_line(&mut age).expect("Could not read age");
-        let age: u8 = age.trim().parse().expect("Could not parse age");
-        // Accepting gender of user
-        println!("\nEnter gender (M/F/O):");
-        let mut gender: String = String::new();
-        io::stdin().read_line(&mut gender).expect("Could not read gender");
-        gender = gender.trim().to_string();
     
-        let user = PersonDetails {
+    // CLUSTER: Introductory message
+    // ----------
+    println!("\n|| Welcome to Find-My-Match");
+    println!("|| Lets start.. (If you're not a user, please register and start again)\n");
+    // ----------
+    
+    // CLUSTER: Already registered or not
+    // ----------
+    println!("|| Are you already registered? [y/n]");
+    let registered: String = utils::user_input("Something went wrong!");
+    // ----------
+
+    // CLUSTER: Reading database file of users [JSON]
+    // ----------
+    let mut all_users = utils::read_user_from_file("db.json").unwrap();
+    // ----------
+    
+    // NOTE: Comparing different types (String with &str) [still works]
+    if registered == "n" {
+        
+        // CLUSTER: Accepting user information
+        // ----------
+        println!("\n-> Enter your name:");
+        let name: String = utils::user_input("Could not read name!"); 
+        
+        println!("\n-> Enter a user_name:");
+        let user_name: String = utils::user_input("Could not read user_name!");    
+        
+        println!("\n-> Enter your age:");
+        let age: String = utils::user_input("Could not read age!");
+        let age: u8 = age.trim().parse().expect("Could not parse age");
+
+        println!("\n-> Enter gender (m/f):");
+        let gender: String = utils::user_input("Could not read gender!");
+        // ----------
+        
+        // CLUSTER: Creating the user object
+        // ----------
+        let user = utils::PersonDetails {
             name: name,
             user_name: user_name,
             age: age,
-            gender: if gender == "M" {
-                Gender::Male
+            gender: if gender == "m" {
+                utils::Gender::Male
             } else {
-                Gender::Female
+                utils::Gender::Female
             },
             preferences: Vec::new()
         };
-        // let j = serde_json::to_string(&user)?;    
-        // let user_json = serde_json::to_string(&user).unwrap();
-        // println!("{}", user_json);
+        // ----------
         
-        // user.register();
-        // let u = read_user_from_file("db.json").unwrap();
-        // echo(u, &Path::new("b.txt")).unwrap_or_else(|why| {
-        //     println!("! {:?}", why.kind());
-        // });
-
-        // let mut all_users = read_user_from_file("db.json").unwrap();
+        // CLUSTER: Adding user to the list of other users
+        // ----------
         all_users.push(user);
         let j = serde_json::to_string_pretty(&all_users).unwrap();
-        file_writer(Path::new("db.json"), &j); 
-        println!("User registered!");
+        utils::file_writer(Path::new("db.json"), &j); 
+        println!("** User registered! **");
+        // ----------
         
-        // println!("{:?}", j);
-        // println!("{:#?}", u);
-
-         // println!("{:#?}", all_users);
-         // Create a vector of all the people details structure
-         // loop through it and output the info
-         // Ask whether u like him or her (Preference)
-         // Take age of the other person
-         
-
-         // println!("{:?}", user);
     } else {
-        // To validate name and username
+        
+        // CLUSTER: To validate name and username
+        // ----------
         let mut is_user: bool = false;
-        let mut user_gender: &Gender = &Gender::Male;
-        println!("\nPlease enter your name and user_name");
+        let mut user_gender: &utils::Gender = &utils::Gender::Male;
+        println!("\n|| Please enter your name and user_name");
         let mut details: Vec<String> = vec!["".to_string(), "".to_string()];
-        println!("\nYour name:");
+        println!("\n-> Your name:");
         io::stdin().read_line(&mut details[0]).expect("Cannot read name");
         details[0] = details[0].trim().to_string();
-        println!("\nYour user_name:");
+        println!("\n-> Your user_name:");
         io::stdin().read_line(&mut details[1]).expect("Could not read user_name");
         details[1] = details[1].trim().to_string();
 
@@ -126,30 +86,36 @@ fn main()  {
                 user_gender = &user.gender;
                 break;
             } else {
-                println!("\nName and user_name doesn't match!");
+                println!("\n:: Name and user_name doesn't match!");
                 break;
             }
         }
+        // ----------
 
+        // CLUSTER: To display all the profiles to the user
+        // ----------
         if is_user {
             let mut liked_or_not: String = String::new();
             let mut choices: Vec<&String> = Vec::new();
-            println!("\nRegistered users:\n");
+            println!("\n(Registered users):\n");
             for user in &all_users {
                 if details[1] != user.user_name && user_gender != &user.gender {
-                    println!("Name: {},\nUsername: {},\nAge: {},\nGender: {:?}\n", 
+                    println!("--------------------");
+                    println!("> Name: {},\n> Username: @{},\n> Age: {},\n> Gender: {:?}\n", 
                              user.name, user.user_name, user.age, user.gender);
-                    println!("Do you like: {}? [y/n]", user.name);
+                    println!("| Do you like: {}? [y/n]", user.name);
                     io::stdin().read_line(&mut liked_or_not).expect("Something went wrong");
                     liked_or_not = liked_or_not.trim().to_string();
                     if liked_or_not == "y" {
                         choices.push(&user.user_name);
                     }
-                    liked_or_not.clear();
+                    liked_or_not.clear();  
+                    println!("--------------------");
                 }
             }
-            println!("\nThe list of user information has exhausted!\n");
-            println!("\nAll your preferences:\n{:?}", choices);
+            println!("\n|| The list of user information has exhausted!");
+            println!("\n|| All your preferences: {:?}", choices);
         }
+        // ----------
     }
 }
---
+++ src/utils.rs
@@ -0,0 +1,68 @@
+
+use std::io;
+use std::fs::{File};
+use serde::{Serialize, Deserialize};
+use std::path::Path;
+use std::io::{BufReader};
+use serde_json;
+use std::error::Error;
+use std::fs;
+
+/* 
+ * Gender enum which holds 2 variants Male & Female. .. 
+ * These variants can be directly stored to a file. ..
+ * These variants will be mapped according to the.. 
+ * selection from terminal / prompt.
+*/
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub enum Gender {
+    Male,
+  	Female
+}
+
+/*
+ * PersonDetails is a struct which holds information ..
+ * about the user.
+*/
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PersonDetails {
+  	pub name: String,
+    pub user_name: String,
+    pub age: u8,
+  	pub gender: Gender,
+    pub preferences: Vec<String>
+}
+
+/*
+ * Function for writing contents to a file. ..
+ * Here its used for writing the details of the new users ..
+ * to the file, which is of JSON format.
+*/
+pub fn file_writer<P: AsRef<Path>, C: AsRef<[u8]>>(path: P, contents: C) -> io::Result<()> {
+    fs::write(path, &contents)?;
+    Ok(())
+}
+
+/*
+ * Function to read all the contents from file. ..
+ * Here its used for reading all the users' information.
+*/
+pub fn read_user_from_file<P: AsRef<Path>>(path: P) -> Result<Vec<PersonDetails>, Box<dyn Error>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let u: Vec<PersonDetails> = serde_json::from_reader(reader)?;
+    Ok(u)
+}
+
+/*
+ * Function for accepting user input from terminal or ..
+ * command prompt. The values are fetched in String type ..
+ * and both leading and trailing spaces are removed using trim() ..
+ * and again converted to String type
+*/
+pub fn user_input(error_msg: &str) -> String {
+    let mut input: String = String::new();
+    io::stdin().read_line(&mut input).expect(error_msg);
+    input = input.trim().to_string();
+    input
+}
